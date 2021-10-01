d92e8ad7feab6a798a3c0d8b6ae6f46c99ad6bdc
Jane Lu <jyl.janelu@gmail.com>
2021-09-30 18:47:43 -0700

    server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,23 @@
 const { User, Comment } = require("../models");
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+
+    me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id })
+            .select('-__v -password')
+            .populate('comments')
+            .populate('friends');
+  
+          return userData;
+        }
+  
+        throw new AuthenticationError('Not logged in');
+    },
+
     users: async () => {
       return User.find()
         .select("-__v -password")
@@ -21,6 +37,12 @@ const resolvers = {
     },
     Comment: async (parent, { _id }) => {
       return Comment.findOne({ _id });
+    },
+
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+
+      return user;
     },
   },
 };
