943d882ad7b03467d6d7b08df97b9138a882d04c
hunterD3CD <hunterd3c@gmail.com>
2021-10-06 18:22:19 -0700

    image list with votes

--- client/src/pages/PhotoVote.js
+++ client/src/pages/PhotoVote.js
@@ -1,7 +1,7 @@
 import React from "react";
 import RatingPhoto from "../components/RatingPhoto";
-// import { useQuery } from "@apollo/react-hooks";
-// import {QUERY_PHOTOS} from "../utils/queries"
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_PHOTOS } from "../utils/queries";
 // ----------------------------------------------------MUI------------------------------------------------------
 // import { Grid} from "@mui/material";
 import ImageList from "@mui/material/ImageList";
@@ -10,37 +10,48 @@ import ImageListItemBar from "@mui/material/ImageListItemBar";
 
 // import { Box, Rating, Typography } from "@mui/material";
 
-
 const PhotoVote = () => {
+  const { loading, data } = useQuery(QUERY_PHOTOS);
+  const photos = data?.photos || [];
+  console.log("photodata:", photos[1]);
 
-  
+  // if data isn't here yet, say so
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
   // ---------------------------------------------------JSX---------------------------------------------------------
   return (
-    <ImageList
-      cols={4}
-      gap={5}
-      xs={{ width: 500, height: 500 }}
-      lg={{ width: 2000, height: 500 }}
-    >
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={item.title}
-            subtitle={<span>by: {item.author}</span>}
-            position="below"
-          />
-          {/* ----------------------------------- RATING COMPONENT----------------------------------------------- */}
-          <RatingPhoto likes={item.likes} dislikes={item.dislikes} />
+    <>
+      <ImageList
+        cols={4}
+        gap={5}
+        xs={{ width: 500, height: 500 }}
+        lg={{ width: 2000, height: 500 }}
+      >
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              src={`${item.img}?w=248&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+            />
+            <ImageListItemBar
+              title={item.title}
+              subtitle={<span>by: {item.author}</span>}
+              position="below"
+            />
+            {/* ----------------------------------- RATING COMPONENT----------------------------------------------- */}
+            <RatingPhoto likes={photos[1].likes} dislikes={photos[1].dislikes} />
           <div>#: users </div>
-        </ImageListItem>
-      ))}
-    </ImageList>
+          </ImageListItem>
+        ))}
+
+        {photos.map((photo) => {
+          return <div>{photo.likes}</div>;
+        })}
+      </ImageList>
+    </>
   );
 };
 
@@ -50,63 +61,85 @@ const itemData = [
   {
     img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
     title: "Breakfast",
-    author: "@bkristastucchio", 
-    likes:"9",
-    dislikes: "5"
+    author: "@bkristastucchio",
+    likes: "9",
+    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
     title: "Burger",
     author: "@rollelflex_graphy726",
+    likes: "9",
+    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
     title: "Camera",
     author: "@helloimnik",
+    likes: "9",
+    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
     title: "Coffee",
     author: "@nolanissac",
+    likes: "9",
+    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
     title: "Hats",
     author: "@hjrc33",
+    likes: "9",
+    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
     title: "Honey",
     author: "@arwinneil",
+    likes: "9",
+    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
     title: "Basketball",
     author: "@tjdragotta",
+    likes: "9",
+    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
     title: "Fern",
     author: "@katie_wasserman",
+    likes: "9",
+    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
     title: "Mushrooms",
     author: "@silverdalex",
+    likes: "9",
+    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
     title: "Tomato basil",
     author: "@shelleypauls",
+    likes: "9",
+    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
     title: "Sea star",
     author: "@peterlaster",
+    likes: "9",
+    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
     title: "Bike",
     author: "@southside_customs",
+    likes: "9",
+    dislikes: "5",
   },
 ];
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,21 +1,21 @@
 import gql from "graphql-tag";
 
-export const GET_ME = gql`
-  {
-    me {
-      _id
-      username
-      email
-      Photo {
-        _id: ID
-        createdAt: String
-        username: String
-        voteCount: Int
-        votes: [Vote]
-      }
-    }
-  }
-`;
+// export const GET_ME = gql`
+//   {
+//     me {
+//       _id
+//       username
+//       email
+//       Photo {
+//         _id: ID
+//         createdAt: String
+//         username: String
+//         voteCount: Int
+//         votes: [Vote]
+//       }
+//     }
+//   }
+// `;
 
 export const QUERY_PHOTOS = gql`
 query photos {
