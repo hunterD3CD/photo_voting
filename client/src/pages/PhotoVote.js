fcdbc6efd19222d978eddc731306a8667cf981d1
hunterD3CD <hunterd3c@gmail.com>
2021-10-07 12:06:23 -0700

    update heroku with image voting and login

--- client/src/pages/PhotoVote.js
+++ client/src/pages/PhotoVote.js
@@ -44,8 +44,8 @@ const PhotoVote = () => {
               position="below"
             />
             {/* ----------------------------------- RATING COMPONENT----------------------------------------------- */}
-            <RatingPhoto likes={photos[index].likes} dislikes={photos[index].dislikes} />
-            <Typography >Vote by: {photos[index].username} </Typography>
+            <RatingPhoto/>
+            <Typography variant="body2" color="text.secondary">Vote by: {photos[index].username} </Typography>
           </ImageListItem>
         ))}
 
@@ -61,84 +61,60 @@ const itemData = [
     img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
     title: "Breakfast",
     author: "@bkristastucchio",
-    likes: "9",
-    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
     title: "Burger",
     author: "@rollelflex_graphy726",
-    likes: "9",
-    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
     title: "Camera",
     author: "@helloimnik",
-    likes: "9",
-    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
     title: "Coffee",
     author: "@nolanissac",
-    likes: "9",
-    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
     title: "Hats",
     author: "@hjrc33",
-    likes: "9",
-    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
     title: "Honey",
     author: "@arwinneil",
-    likes: "9",
-    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
     title: "Basketball",
     author: "@tjdragotta",
-    likes: "9",
-    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
     title: "Fern",
     author: "@katie_wasserman",
-    likes: "9",
-    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
     title: "Mushrooms",
     author: "@silverdalex",
-    likes: "9",
-    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
     title: "Tomato basil",
     author: "@shelleypauls",
-    likes: "9",
-    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
     title: "Sea star",
     author: "@peterlaster",
-    likes: "9",
-    dislikes: "5",
   },
   {
     img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
     title: "Bike",
     author: "@southside_customs",
-    likes: "9",
-    dislikes: "5",
   },
 ];
